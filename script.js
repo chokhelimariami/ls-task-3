de5290338442756f1e2d70f4d53ddeda3a33efed
chokhelimariami <chokhelimariami618@gmail.com>
2024-11-11 14:39:10 +0100

    js task 3

---
+++ script.js
@@ -0,0 +1,16 @@
+let age = 20;
+
+
+if( age<=12 && age>0) {
+    console.log("ticket price is $5");
+} else if (age <= 17){
+    console.log("ticket price is $10");
+}else if (age<=64) {
+    console.log("ticket price is $15");
+}else if(age >=65 && age <120) {
+    console.log("ticket price is $8");
+}else  {
+    console.log("wrong age");
+
+}
+ 
